d3718adc5eaabfe63a83323a073deb99bcdcba53
jiaw.hu <h96599021@gmail.com>
2023-11-01 17:39:34 +0800

    chore: test conversation

--- textcraft/api/test.py
+++ textcraft/api/test.py
@@ -1,6 +1,8 @@
+from cgi import test
 from fastapi import APIRouter
 
 from textcraft.core.settings import settings
+from textcraft.chains.conversation import Conversation
 
 test_router = APIRouter()
 
@@ -8,3 +10,7 @@ test_router = APIRouter()
 @test_router.get("/default_model")
 async def get_config():
     return settings.DEFAULT_LLM
+
+@test_router.get("/conversation")
+async def conversation(text: str):
+    return Conversation().chatForText(text)
---
+++ textcraft/chains/conversation.py
@@ -0,0 +1,43 @@
+import textwrap
+import time
+
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+
+from textcraft.models.llms.llm_creator import LLMCreator
+
+llm = LLMCreator.create_llm()
+
+memory = ConversationBufferWindowMemory(k=3)  # k=1,意味着只能记住最后1轮对话内容
+conversation = ConversationChain(llm=llm, verbose=True, memory=memory)
+
+
+def output_response(response: str) -> None:
+    if not response:
+        exit(0)
+    for line in textwrap.wrap(response, width=60):
+        for word in line.split():
+            for char in word:
+                print(char, end="", flush=True)
+                time.sleep(0.1)  # Add a delay of 0.1 seconds between each character
+            print(" ", end="", flush=True)  # Add a space between each word
+        print()  # Move to the next line after each line is printed
+    print("----------------------------------------------------------------")
+
+
+class Conversation:
+    def chatForText(self, text):
+        result = conversation.run(text)
+        return result
+
+
+if __name__ == "__main__":
+    chat = Conversation()
+
+    while True:
+        try:
+            user_input = input("请输入您的问题：")
+            response = chat.chatForText(user_input)
+            output_response(response)
+        except KeyboardInterrupt:
+            break
