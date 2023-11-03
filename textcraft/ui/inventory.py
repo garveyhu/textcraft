0f35fec5e23325cf94e07a166d0d512799431856
jiaw.hu <h96599021@gmail.com>
2023-11-03 11:00:34 +0800

    chore: ui

---
+++ textcraft/ui/inventory.py
@@ -0,0 +1,12 @@
+class Inventory:
+    llm = [
+        "openai",
+        "qwen",
+        "ernie",
+        "spark",
+        "baichuan",
+    ]
+    embeddings = ["openai", "qwen"]
+
+
+inventory = Inventory()
--- textcraft/ui/streamlit.py
+++ textcraft/ui/streamlit.py
@@ -1,14 +1,12 @@
 import streamlit as st
 
-from textcraft.core.inventory import inventory
+from textcraft.ui.inventory import inventory
 
 st.title("Configurations")
 
-selected_vector_store = st.selectbox("vector_store:", inventory.vector_store)
 selected_llm = st.selectbox("llm:", inventory.llm)
 selected_embedding = st.selectbox("embeddings:", inventory.embeddings)
 
 if st.button("load"):
-    st.write(f"已选择向量存储: {selected_vector_store}")
     st.write(f"已选择LLM模型: {selected_llm}")
     st.write(f"已选择嵌入模型: {selected_embedding}")
