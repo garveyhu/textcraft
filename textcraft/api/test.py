b92889bbd8e933d0cf53f99af76730e2aadd66ca
jiaw.hu <h96599021@gmail.com>
2023-11-06 14:28:32 +0800

    Update test.py

--- textcraft/api/test.py
+++ textcraft/api/test.py
@@ -1,15 +1,9 @@
-from cgi import test
 from fastapi import APIRouter
 
-from textcraft.core.settings import settings
 from textcraft.chains.conversation import Conversation
 
 test_router = APIRouter()
 
-
-@test_router.get("/default_model")
-async def get_config():
-    return settings.DEFAULT_LLM
 
 @test_router.get("/conversation")
 async def conversation(text: str):
