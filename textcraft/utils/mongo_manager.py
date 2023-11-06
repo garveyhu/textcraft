468eca4ce599c598fae6c93ba3a3a0e5be899488
jiaw.hu <h96599021@gmail.com>
2023-11-06 14:31:03 +0800

    feat: add mongodb and redis manager

---
+++ textcraft/utils/mongo_manager.py
@@ -0,0 +1,69 @@
+import json
+import re
+
+import pymongo
+from bson import json_util
+
+from textcraft.core.settings import config_file, settings
+
+
+class MongoDBManager:
+    def __init__(self) -> None:
+        self.client = pymongo.MongoClient(settings.MONGODB_URL)
+        self.db = self.client[settings.MONGODB_DB]
+        self.collection = self.db[settings.MONGODB_COLLECTION]
+
+    def insert_config(self, config_data):
+        result = self.collection.insert_one(config_data)
+        return result.inserted_id
+
+    def update_config(self, config_data):
+        user_id = config_data["user"]["id"]
+        filter_query = {"user.id": user_id}
+        update_data = {"$set": config_data}
+        result = self.collection.update_one(filter_query, update_data)
+        return result.modified_count
+
+    def delete_config(self, user_id):
+        filter_query = {"user.id": user_id}
+        result = self.collection.delete_one(filter_query)
+        return result.deleted_count
+
+    def get_config(self, user_id):
+        filter_query = {"user.id": user_id}
+        config_data = json_util.loads(json_util.dumps(self.collection.find_one(filter_query)))
+        if "_id" in config_data:
+                del config_data["_id"]
+                
+        return config_data
+    
+    def all_config(self):
+        all_config_data = list(self.collection.find())
+        json_config_data = []
+        
+        if all_config_data:
+            for config_data in all_config_data:
+                if "_id" in config_data:
+                    del config_data["_id"]
+                json_config_data.append(json_util.loads(json_util.dumps(config_data)))
+                
+        return json_config_data
+
+
+    def close_connection(self):
+        self.client.close()
+
+    def load_json(self):
+        with open(config_file, "r") as file:
+            config_data = json.load(file)
+        return config_data
+
+
+if __name__ == "__main__":
+    collection = MongoDBManager()
+    # print(collection.load_json())
+    # print(collection.insert_config(collection.load_json()))
+    # print(collection.update_config(collection.load_json()))
+    # print(collection.delete_config("030317"))
+    # print(collection.get_config("030317"))
+    # print(collection.all_config())
---
+++ textcraft/utils/redis_manager.py
@@ -0,0 +1,53 @@
+import json
+
+import redis
+from bson import json_util
+
+from textcraft.core.settings import settings
+from textcraft.utils.mongo_manager import MongoDBManager
+
+
+class RedisManager:
+    def __init__(self):
+        self.client = redis.StrictRedis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            db=settings.REDIS_DB,
+            password=settings.REDIS_PASSWORD,
+        )
+
+    def sync_configs_to_redis(self):
+        mongo_manager = MongoDBManager()
+        all_config_data = mongo_manager.all_config()
+
+        if all_config_data:
+            for config_data in all_config_data:
+                self.update_config(config_data)
+
+    def update_config(self, config_data):
+        user_id = config_data["user"]["id"]
+        key = f"user:{user_id}"
+        result = self.client.set(key, json.dumps(config_data))
+        return result
+
+    def delete_config(self, user_id):
+        key = f"user:{user_id}"
+        result = self.client.delete(key)
+        return result
+
+    def get_config(self, user_id):
+        key = f"user:{user_id}"
+        config_data = self.client.get(key)
+        if config_data:
+            return json.loads(config_data)
+        else:
+            return None
+
+    def close_connection(self):
+        self.client.connection_pool.disconnect()
+
+
+if __name__ == "__main__":
+    redis_manager = RedisManager()
+    # redis_manager.sync_configs_to_redis()
+    # print(redis_manager.get_config("030317"))
