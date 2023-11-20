b7f679440b996bdd13cf2fd723f4004a93b960d1
jiaw.hu <h96599021@gmail.com>
2023-11-20 11:28:00 +0800

    Update check_requirements.py

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -1,15 +1,19 @@
-import os
+import subprocess
 import sys
 
-
-def main():
+def install_poetry_and_add_source():
     try:
-        import poetry.factory
+        import poetry
     except ModuleNotFoundError:
-        os.system(
-            f"{sys.executable} -m pip install poetry -i https://pypi.tuna.tsinghua.edu.cn/simple"
-        )
+        subprocess.run([
+            sys.executable, "-m", "pip", "install", "poetry",
+            "-i", "https://pypi.tuna.tsinghua.edu.cn/simple"
+        ], check=True)
 
+    # Add the source with the priority set to "default"
+    subprocess.run([
+        "poetry", "source", "add", "--priority=default", "mirrors", "https://pypi.tuna.tsinghua.edu.cn/simple/"
+    ], check=True)
 
 if __name__ == "__main__":
-    main()
+    install_poetry_and_add_source()
